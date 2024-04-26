6d4befee64e5d73613f62a9da7ecc659607030d2
luthfi <luthfinassirarayalan@gmail.com>
2024-04-26 19:01:24 +0530

    dateRange

--- StockPulse.py
+++ StockPulse.py
@@ -2,6 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 import plotly.graph_objects as go
+import datetime
 
 st.set_page_config(page_title="StockPulse",
                    page_icon=":bar_chart:",
@@ -16,14 +17,11 @@ theme = """
                     padding: 1rem 3rem 2rem;
                     color: #FFFFFF;
                 }  
-                .st-cd {
+                .st-d3 {
                     background-color: #118d95;
                 }
                 p {
                     color: #699eb8;
-                }
-                .st-cu {
-                    # background: linear-gradient(to right, #699eb8 100%, #699eb8 100%, #699eb8 100%, #699eb8 100%);
                 }
                 
             </style>
@@ -41,17 +39,28 @@ st.markdown("<h1 style='text-align: center; color:#ffffff;'> StockPlex: Dynamic 
 st.markdown("##")
 
 
-year = st.select_slider(
-    "Select a from which year onwards ",
-    options=['2024', '2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010'],
-    )
 
 #KPI
-df_2023_onw = df[df['Date'].dt.year == int(year)] # Filter data starting from 2024 onwards
-average_price = round(df_2023_onw['Open'].mean(), 2)
-average_high = round(df_2023_onw['High'].mean(), 2)
-average_low = round(df_2023_onw['Low'].mean(), 2)
-average_volume = int(df_2023_onw['Volume'].mean())
+startDate = datetime.date(2010, 7, 18)
+endDate = datetime.date(2024, 3, 19)
+
+
+d = st.date_input(
+    "Select Date Range",
+    (datetime.date(2024, 1, 1), endDate),
+    startDate,
+    endDate,
+    format="MM.DD.YYYY",
+)
+
+start_date = datetime.datetime(d[0].year, d[0].month, d[0].day)  # Convert to datetime
+end_date = datetime.datetime(d[1].year, d[1].month, d[1].day)  # Convert to datetime
+df_filtered = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+
+average_price = round(df_filtered['Open'].mean(), 2)
+average_high = round(df_filtered['High'].mean(), 2)
+average_low = round(df_filtered['Low'].mean(), 2)
+average_volume = int(df_filtered['Volume'].mean())
 
 
 
@@ -84,32 +93,32 @@ with tab1:
     col1,col2,col3,col4=st.columns(4) # to get columns
 
     with col1:
-        open_gr = df_2023_onw.groupby(['Date'])['Open'].mean().reset_index()
+        open_gr = df_filtered.groupby(['Date'])['Open'].mean().reset_index()
         fig_line = px.line(open_gr, x='Date', y='Open', title='Open Price') # Plotting the line chart
         st.plotly_chart(fig_line, use_container_width=True)
 
     with col2:
-        high_gr = df_2023_onw.groupby(['Date'])['High'].mean().reset_index()
+        high_gr = df_filtered.groupby(['Date'])['High'].mean().reset_index()
         fig_line = px.line(high_gr, x='Date', y='High', title='High Price') # Plotting the line chart
         st.plotly_chart(fig_line, use_container_width=True)
 
     with col3:
-        low_gr = df_2023_onw.groupby(['Date'])['Low'].mean().reset_index()
+        low_gr = df_filtered.groupby(['Date'])['Low'].mean().reset_index()
         fig_line = px.line(low_gr, x='Date', y='Low', title='Low Price') # Plotting the line chart
         st.plotly_chart(fig_line, use_container_width=True)
 
     with col4:
-        close_gr = df_2023_onw.groupby(['Date'])['Close'].mean().reset_index()
+        close_gr = df_filtered.groupby(['Date'])['Close'].mean().reset_index()
         fig_line = px.line(close_gr, x='Date', y='Close', title='Close Price') # Plotting the line chart
         st.plotly_chart(fig_line, use_container_width=True)
         
 
 with tab2:
-    candle = go.Figure(data=[go.Candlestick(x=df_2023_onw['Date'],
-                                            open=df_2023_onw['Open'],
-                                            high=df_2023_onw['High'],
-                                            low=df_2023_onw['Low'],
-                                            close=df_2023_onw['Close'])])
+    candle = go.Figure(data=[go.Candlestick(x=df_filtered['Date'],
+                                            open=df_filtered['Open'],
+                                            high=df_filtered['High'],
+                                            low=df_filtered['Low'],
+                                            close=df_filtered['Close'])])
     candle.update_layout(title="Candlestick Chart for Daily Stock Prices")
     st.plotly_chart(candle, use_container_width=True)  
 
@@ -119,7 +128,7 @@ st.markdown("---")
 
 # Group by date
 vol_month = (
-    df_2023_onw.groupby(by=["Date"])["Volume"]
+    df_filtered.groupby(by=["Date"])["Volume"]
     .mean()
     .reset_index()
     .sort_values(by="Date")
